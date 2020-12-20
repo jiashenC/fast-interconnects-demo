a966966e3f470eec2cddffd6c85c579e61ad3bcc
jiashenC <caojiashen24@gmail.com>
2020-12-20 17:08:20 -0500

    sequential scan

--- ccgpu-demo.cu
+++ ccgpu-demo.cu
@@ -4,50 +4,55 @@
 
 // Add a scalar to the vector
 __global__ void vadd(int *const v, int const a, size_t const len) {
-    const unsigned int gid = blockDim.x * blockIdx.x + threadIdx.x;
-    const unsigned int gsize = gridDim.x * blockDim.x;
+  const unsigned int gid = blockDim.x * blockIdx.x + threadIdx.x;
+  const unsigned int gsize = gridDim.x * blockDim.x;
 
-    for (size_t i = gid; i < len; i += gsize) {
-        v[i] += a;
-    }
+  for (size_t i = gid; i < len; i += gsize) {
+    v[i] += a;
+  }
 }
 
 int main() {
-    // Vector length
-    constexpr size_t LEN = 100'000;
+  // Vector length
+  constexpr size_t LEN = 100'000;
 
-    // GPU kernel parameters
-    constexpr unsigned int grid = 160;
-    constexpr unsigned int block = 1024;
+  // GPU kernel parameters
+  constexpr unsigned int grid = 160;
+  constexpr unsigned int block = 1024;
 
-    // Allocate vector
-    int *data = nullptr;
-    data = reinterpret_cast<int *>(malloc(LEN * sizeof(int)));
-    if (data == nullptr) {
-        std::cerr << "Failed to allocate memory" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
+  // Allocate vector
+  std::cout << "Allocate vector on CPU" << std::endl;
+  int *data = nullptr;
+  data = reinterpret_cast<int *>(malloc(LEN * sizeof(int)));
+  if (data == nullptr) {
+    std::cerr << "Failed to allocate memory" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
 
-    // Initialize vector with some data
-    for (size_t i = 0; i < LEN; ++i) {
-        data[i] = i;
-    }
+  // Initialize vector with some data
+  std::cout << "Init vector on CPU" << std::endl;
+  for (size_t i = 0; i < LEN; ++i) {
+    data[i] = i;
+  }
 
-    // Call a function to do some work
-    vadd<<<grid, block>>>(data, 1, LEN);
+  // Call a function to do some work
+  std::cout << "Process on GPU" << std::endl;
+  vadd<<<grid, block>>>(data, 1, LEN);
 
-    // Wait for the GPU kernel to finish execution
-    cudaDeviceSynchronize();
+  // Wait for the GPU kernel to finish execution
+  cudaDeviceSynchronize();
 
-    // Verify that result is correct
-    unsigned long long sum = 0;
-    for (size_t i = 0; i < LEN; ++i) {
-        sum += data[i];
-    }
-    assert(sum == (LEN * (LEN + 1)) / 2);
+  // Verify that result is correct
+  std::cout << "Get and verify results" << std::endl;
+  unsigned long long sum = 0;
+  for (size_t i = 0; i < LEN; ++i) {
+    sum += data[i];
+  }
+  assert(sum == (LEN * (LEN + 1)) / 2);
 
-    // Free vector
-    free(data);
+  // Free vector
+  std::cout << "Free memory" << std::endl;
+  free(data);
 
-    std::exit(EXIT_SUCCESS);
+  std::exit(EXIT_SUCCESS);
 }
--- cuda-demo.cu
+++ cuda-demo.cu
@@ -1,66 +1,81 @@
 #include <iostream>
+#include <chrono>
 #include <cstdlib>
 #include <cassert>
 
 // Add a scalar to the vector
-__global__ void vadd(int *const v, int const a, size_t const len) {
-    const unsigned int gid = blockDim.x * blockIdx.x + threadIdx.x;
-    const unsigned int gsize = gridDim.x * blockDim.x;
+__global__ void vadd(int* const v, int const a, size_t const len) {
+  const unsigned int gid = blockDim.x * blockIdx.x + threadIdx.x;
+  const unsigned int gsize = gridDim.x * blockDim.x;
 
-    for (size_t i = gid; i < len; i += gsize) {
-        v[i] += a;
-    }
+  for (size_t i = gid; i < len; i += gsize) {
+    v[i] += a;
+  }
 }
 
-int main() {
-    // Vector length
-    constexpr size_t LEN = 100'000;
+int main(int argc, char* argv[]) {
+  // GPU device ID
+  const int gpu_id = 0;
+  cudaSetDevice(gpu_id);
 
-    // GPU kernel parameters
-    constexpr unsigned int grid = 160;
-    constexpr unsigned int block = 1024;
+  // Vector length
+  size_t LEN = std::stol(argv[1]);
 
-    // Allocate vector
-    int *data = nullptr;
+  // GPU kernel parameters
+  int grid;
+  int block;
+  cudaOccupancyMaxPotentialBlockSize(&grid, &block, vadd, 0, 0);
 
-    // GPU device memory
-    //
-    // cudaMalloc(&data, LEN * sizeof(int));
+  // Allocate vector
+  int* h_data = static_cast<int*>(malloc(sizeof(int) * LEN));
+  for (size_t i = 0; i < LEN; ++i) {
+    h_data[i] = 1;
+  }
+  int* d_data;
 
-    // Unified Virtual Addressing
-    // aka: "pinned memory"
-    //
-    cudaHostAlloc(&data, LEN * sizeof(int), 0);
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    // Unified Memory
-    //
-    // cudaMallocManaged(&data, LEN * sizeof(int));
+#if CUDA_UM
+  cudaMallocManaged(&d_data, LEN * sizeof(int));
+  cudaMemAdvise(d_data, LEN * sizeof(int), cudaMemAdviseSetAccessedBy, gpu_id);
+  memcpy(d_data, h_data, LEN * sizeof(int));
+#elif CUDA_ZEROCOPY
+  cudaHostRegister(h_data, LEN * sizeof(int), cudaHostRegisterMapped);
+  cudaHostGetDevicePointer(&d_data, h_data, 0);
+#else
+  cudaMalloc(&d_data, LEN * sizeof(int));
+  cudaMemcpy(d_data, h_data, sizeof(int) * LEN, cudaMemcpyHostToDevice);
+#endif
 
-    if (data == nullptr) {
-        std::cerr << "Failed to allocate memory" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
+  // Call a function to do some work
+  vadd<<<grid, block>>>(d_data, 1, LEN);
+  cudaDeviceSynchronize();
 
-    // Initialize vector with some data
-    for (size_t i = 0; i < LEN; ++i) {
-        data[i] = i;
-    }
+#if CUDA_UM
+  memcpy(h_data, d_data, LEN * sizeof(int));
+#elif CUDA_ZEROCOPY
+#else
+  cudaMemcpy(h_data, d_data, sizeof(int) * LEN, cudaMemcpyDeviceToHost);
+#endif
 
-    // Call a function to do some work
-    vadd<<<grid, block>>>(data, 1, LEN);
+  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+  std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
 
-    // Wait for the GPU kernel to finish execution
-    cudaDeviceSynchronize();
+  // Verify that result is correct
+  unsigned long long sum = 0;
+  for (size_t i = 0; i < LEN; ++i) {
+    sum += h_data[i];
+  }
+  assert(sum / 2 == LEN);
 
-    // Verify that result is correct
-    unsigned long long sum = 0;
-    for (size_t i = 0; i < LEN; ++i) {
-        sum += data[i];
-    }
-    assert(sum == (LEN * (LEN + 1)) / 2);
+#ifdef CUDA_UM
+  cudaFree(d_data);
+#elif CUDA_ZEROCOPY
+  cudaHostUnregister(d_data);
+  h_data = nullptr;
+#else
+  cudaFree(d_data);
+#endif
 
-    // Free CUDA-allocated vector
-    cudaFree(data);
-
-    std::exit(EXIT_SUCCESS);
+  std::exit(EXIT_SUCCESS);
 }
